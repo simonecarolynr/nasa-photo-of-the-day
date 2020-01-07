0be435faf725e73f99d14ac8e708a6d717e15bfc
simonecarolynr <simonecarolynr@gmail.com>
2020-01-06 20:59:32 -0700

    MVP

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
+import axios from 'axios';
 
 function App() {
+  const [state, setState] = useState([]);
+
+  useEffect(() => {
+      axios
+      .get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
+      .then (response => {
+          setState(response.data)
+          console.log(response)
+      })
+      .catch (err => {
+          console.log(err)
+      })
+  }, []);
+ 
   return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
-    </div>
-  );
+      <div>        
+          <img className='photo' src={state.url}/>
+          <h1 className='title'> {state.title}</h1>
+          <h3 className='date'>{state.date}</h3>
+      </div>
+
+  )
 }
 
 export default App;
